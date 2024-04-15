63aee1fa10997f5c8409369d85ef4a43795dcb6f
bulbigood <bulbigood@gmail.com>
2024-04-15 10:26:24 +0200

    Rename Android artifacts

--- cordova/scripts/create-config-from-template.sh
+++ cordova/scripts/create-config-from-template.sh
@@ -11,9 +11,8 @@ if [ -f "../config/$TEMPLATE_FILE" ]; then
 
   export HTML_ENTRYPOINT="index.$ENVIRONMENT-$PLATFORM.html"
   export PACKAGE_VERSION="$(cat ../../package.json | ../node_modules/.bin/json version)"
-  aligned_build_version=$(($(TZ=UTC date +%s)-1708020000))
-  export ANDROID_VERSIONCODE="$aligned_build_version"
-  export IOS_BUNDLE_VERSION="$PACKAGE_VERSION.$aligned_build_version"
+  export BUILD_VERSION="$(($(TZ=UTC date +%s)-1708020000))"
+  export IOS_BUNDLE_VERSION="$PACKAGE_VERSION.$BUILD_VERSION"
 
   if [ "$PACKAGE_VERSION" == "" ]; then
     echo "Error: Could not read app version." 1>&2
