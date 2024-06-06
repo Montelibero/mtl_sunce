5f752168a871327ea2879fc4fd7fd4ed97c8cfd2
bulbigood <bulbigood@gmail.com>
2024-06-06 23:58:02 +0200

    test

--- cordova/scripts/create-config-from-template.sh
+++ cordova/scripts/create-config-from-template.sh
@@ -9,10 +9,19 @@ cd "$(dirname $0)"
 if [ -f "../config/$TEMPLATE_FILE" ]; then
   echo "Creating config file from $TEMPLATE_FILE ..."
 
-  export HTML_ENTRYPOINT="index.$ENVIRONMENT-$PLATFORM.html"
-  export PACKAGE_VERSION="$(cat ../../package.json | ../node_modules/.bin/json version)"
-  export BUILD_VERSION="$(($(TZ=UTC date +%s)-1708020000))"
+  # Check if versions are already provided
+  if [ "$PACKAGE_VERSION" == "" ]; then
+    echo "Package version is not provided. Reading from package.json..."
+    export PACKAGE_VERSION="$(cat ../../package.json | ../node_modules/.bin/json version)"
+  fi
+  if [ "$BUILD_VERSION" == "" ]; then
+    echo "Build version is not provided. Generating..."
+    export BUILD_VERSION="$(($(TZ=UTC date +%s)-1710000000))"
+  fi
+  echo "Generate config version $PACKAGE_VERSION.$BUILD_VERSION"
+
   export IOS_BUNDLE_VERSION="$PACKAGE_VERSION.$BUILD_VERSION"
+  export HTML_ENTRYPOINT="index.$ENVIRONMENT-$PLATFORM.html"
 
   if [ "$PACKAGE_VERSION" == "" ]; then
     echo "Error: Could not read app version." 1>&2
